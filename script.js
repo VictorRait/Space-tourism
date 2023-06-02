fd03d4a49e71a8095651476e5e18837e86764223
VictorRait <115611038+VictorRait@users.noreply.github.com>
2023-06-02 14:18:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -148,7 +148,7 @@ if (crewPage) {
         " Douglas Gerald Hurley is an American engineer, former Marine Corps pilot" +
         "and former NASA astronaut. He launched into space for the third time as " +
         "commander of Crew Dragon Demo-2.",
-      img: "assets\\crew\\image-douglas-hurley.png",
+      img: "..\\assets\\crew\\image-douglas-hurley.png",
     },
     {
       name: "Anousheh Ansari",
@@ -157,7 +157,7 @@ if (crewPage) {
         " Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. " +
         " Ansari was the fourth self-funded space tourist, the first self-funded woman to " +
         "fly to the ISS, and the first Iranian in space. ",
-      img: "assets\\crew\\image-anousheh-ansari.png",
+      img: "..\\assets\\crew\\image-anousheh-ansari.png",
     },
     {
       name: " Victor Glover",
@@ -167,7 +167,7 @@ if (crewPage) {
         "International Space Station. Glover is a commander in the U.S. Navy where " +
         "he pilots an F/A-18.He was a crew member of Expedition 64, and served as a " +
         "station systems flight engineer.",
-      img: "assets\\crew\\image-victor-glover.png",
+      img: "..\\assets\\crew\\image-victor-glover.png",
     },
     {
       name: "Mark Shuttleworth",
@@ -176,7 +176,7 @@ if (crewPage) {
         "Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind " +
         "the Linux-based Ubuntu operating system. Shuttleworth became the first South " +
         "African to travel to space as a space tourist.",
-      img: "assets\\crew\\image-mark-shuttleworth.png",
+      img: "..\\assets\\crew\\image-mark-shuttleworth.png",
     },
   ];
 
@@ -238,7 +238,7 @@ if (technologyPage) {
         "payload from Earth's surface to space, usually to Earth orbit or beyond. Our" +
         "WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall," +
         "it's quite an awe-inspiring sight on the launch pad!",
-      img: "assets\\technology\\image-launch-vehicle-portrait.jpg",
+      img: "..\\assets\\technology\\image-launch-vehicle-portrait.jpg",
     },
     {
       tech: "Spaceport",
@@ -248,7 +248,7 @@ if (technologyPage) {
         " by analogy to the seaport for ships or airport for aircraft. Based in the " +
         "famous Cape Canaveral, our spaceport is ideally situated to take advantage " +
         "of the Earth’s rotation for launch.",
-      img: "assets\\technology\\image-spaceport-portrait.jpg",
+      img: "..\\assets\\technology\\image-spaceport-portrait.jpg",
     },
     {
       tech: "Space Capsule",
@@ -257,7 +257,7 @@ if (technologyPage) {
         "capsule to reenter the Earth's atmosphere without wings. Our capsule is where " +
         "you'll spend your time during the flight. It includes a space gym, cinema," +
         "and plenty of other activities to keep you entertained.",
-      img: "assets\\technology\\image-space-capsule-portrait.jpg",
+      img: "..\\assets\\technology\\image-space-capsule-portrait.jpg",
     },
   ];
 
@@ -282,11 +282,11 @@ if (technologyPage) {
         setData(0);
       
         if (window.matchMedia("max-width: 900px")) {
-          imgContainer.style.backgroundImage = "url(assets/technology/image-launch-vehicle-landscape.jpg)";
+          imgContainer.style.backgroundImage = "url(../assets/technology/image-launch-vehicle-landscape.jpg)";
           
         }
         if (window.matchMedia("max-width: 545px")) {
-          imgContainer.style.backgroundImage = "url(assets/technology/image-launch-vehicle-portrait.jpg)";
+          imgContainer.style.backgroundImage = "url(../assets/technology/image-launch-vehicle-portrait.jpg)";
             imgContainer.style.backgroundPosition ="0 bottom";
         } 
           
@@ -298,7 +298,7 @@ if (technologyPage) {
       if (btn.dataset.num === "2") {
         setData(1);
         if (window.matchMedia("max-width: 900px")) {
-          imgContainer.style.backgroundImage = "url(/assets/technology/image-spaceport-landscape.jpg )";
+          imgContainer.style.backgroundImage = "url(../assets/technology/image-spaceport-landscape.jpg )";
           imgContainer.style.backgroundPosition = "center center";
         }
       }
@@ -307,7 +307,7 @@ if (technologyPage) {
         setData(2);
         if (window.matchMedia("max-width: 900px")) {
           imgContainer.style.backgroundImage =
-            "url(assets/technology/image-space-capsule-landscape.jpg )";
+            "url(../assets/technology/image-space-capsule-landscape.jpg )";
             imgContainer.style.backgroundPosition = "center center";
         }
       }
